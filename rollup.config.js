904a5805c97692c1d6ba22c9de286d24822997fb
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2018-01-29 07:45:45 +0300

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,26 @@
+import replace from 'rollup-plugin-replace'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+
 export default {
   input: 'src/index.js',
   output: {
     file: 'build/main.js',
     format: 'iife'
-  }
+  },
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    resolve({
+      jsnext: true
+    }),
+    commonjs({
+      include: 'node_modules/**'
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ]
 }
