1c0b142f18220a49fbec8707166ebb5ddd41e0cc
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2018-01-29 08:07:17 +0300

    Update app with apollo provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from 'react-apollo'
+
+const client = new ApolloClient({
+  link: new HttpLink(),
+  cache: new InMemoryCache()
+})
 
 const App = () => (
-  <p>hey</p>
+  <ApolloProvider client={client}>
+    <p>hey</p>
+  </ApolloProvider>
 )
 
 export default App
