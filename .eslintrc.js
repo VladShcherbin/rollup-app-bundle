f4580c7c477059ad261f8340ece2949bd75d3173
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2018-01-29 07:46:07 +0300

    Update eslint config with react, browser rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,10 @@ module.exports = {
   extends: 'airbnb',
   rules: {
     'comma-dangle': ['error', 'never'],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
     semi: ['error', 'never']
+  },
+  env: {
+    browser: true
   }
 }
